3121ec912567e7d6fa5093c78b0c28d16c982af4
Hardik Bajaj <HardikBajaj364@gmail.com>
2026-04-04 18:57:11 +0530

    Revert "merge: bring Jashan's multi-workspace restructure into qa"
    This reverts commit e60cb5d4e6144b4cd71805d445571ff0b1d740de, reversing
    changes made to 90e9eef8f983a110ba840e2fac831e5dc08e2156.

--- kron-mobile/ios/Runner/Runner-Bridging-Header.h
+++
@@ -1,8 +0,0 @@
-//
-//  Runner-Bridging-Header.h
-//  KRON SIEM
-//
-//  Exposes Objective-C Flutter plugin registrar to Swift.
-//  This file is referenced by the Xcode project SWIFT_OBJC_BRIDGING_HEADER setting.
-
-#import "GeneratedPluginRegistrant.h"
--- kron-standard/crates/kron-ctl/src/cmd/mod.rs
+++
@@ -1,7 +0,0 @@
-//! Command modules for `kron-ctl`.
-
-pub mod agents;
-pub mod events;
-pub mod health;
-pub mod migration;
-pub mod storage;
